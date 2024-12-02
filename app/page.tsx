a0b219083309e5aded7b8fb17b524b3d2c82da38
christianHadinata <christianhadinata2004@gmail.com>
2024-12-02 09:05:09 +0700

    feat: get data sidang from server

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useAuth } from "@/app/utils/AuthContext";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 import {
   Button,
   Select,
@@ -17,74 +18,49 @@ import {
 } from "@nextui-org/react";
 import Link from "next/link";
 
-type UserDetails = {
-  email: string;
-  nama: string;
-  role: string;
+type DataSidangTypes = {
+  idx: number;
+  idSidang: number;
+  judulSkripsi: string;
+  TA: number;
+  namaMahasiswa: string;
+  npm: string;
+  emailDosen: string;
+  roleDosen: string;
 };
 
 export default function Home() {
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState<string | null>(null);
   const { user, loading } = useAuth();
   const router = useRouter();
+  const [dataSidang, setDataSidang] = useState<DataSidangTypes[]>();
 
   useEffect(() => {
     if (!loading && !user) {
       router.push("/login");
     }
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        "http://localhost:5000/api/sidang/listSidang",
+        {
+          params: {
+            email: user?.email,
+            role: user?.role,
+          },
+        }
+      );
+
+      setDataSidang(data);
+    };
+
+    if (user) {
+      fetchData();
+    }
     console.log(user);
   }, [loading, user, router]);
 
-  if (loading) {
-    return (
-      <div
-        role="status"
-        className="h-screen w-full items-center justify-center bg-blue-50 flex "
-      >
-        <svg
-          aria-hidden="true"
-          className="inline w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-          viewBox="0 0 100 101"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-            fill="currentColor"
-          />
-          <path
-            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-            fill="currentFill"
-          />
-        </svg>
-        {/* <span className="sr-only">Loading...</span> */}
-      </div>
-    );
-  }
+  useEffect(() => {}, []);
 
-  // if (error) {
-  //   return (
-  //     <div className=" h-screen w-full items-center justify-center bg-blue-50 flex flex-col gap-y-7">
-  //       <h1 className="text-red-500 text-4xl font-bold">{error}</h1>
-  //       <p className="text-2xl">Please re-login</p>
-  //       <Button color="primary" size="lg" as={Link} href="/login">
-  //         Log in
-  //       </Button>
-  //     </div>
-  //   );
-  // }
-
-  type DataTypes = {
-    idx: number;
-    idSidang: number;
-    judulSkripsi: string;
-    nama: string;
-    npm: string;
-    ta: number;
-  };
-
-  const data: DataTypes[] = [];
+  // const data: DataSidangTypes[] = [];
 
   const columns = [
     {
@@ -112,6 +88,35 @@ export default function Home() {
       label: "VIEW",
     },
   ];
+  const data = dataSidang?.map((item, index) => {
+    return { ...item, idx: index + 1 };
+  });
+
+  if (loading) {
+    return (
+      <div
+        role="status"
+        className="h-screen w-full items-center justify-center bg-blue-50 flex "
+      >
+        <svg
+          aria-hidden="true"
+          className="inline w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -211,22 +216,42 @@ export default function Home() {
                   )}
                 </TableHeader>
                 <TableBody
-                  items={data}
+                  items={data || []}
                   emptyContent={"Data Sidang Skripsi Masih Kosong"}
                 >
-                  {/* {(item) => (
-                    <TableRow key={item.idx} className="">
-                      <TableCell className="text-center font-medium">
-                        {item.idx}
-                      </TableCell>
+                  {(item) => {
+                    console.log(item);
+                    return (
+                      <TableRow key={item.idx} className="">
+                        <TableCell className="text-center font-medium max-w-10">
+                          {item.idx}
+                        </TableCell>
+                        <TableCell className="text-center font-medium max-w-40 break-words">
+                          {item.judulSkripsi}
+                        </TableCell>
+                        <TableCell className="text-center font-medium max-w-32 break-words">
+                          {item.namaMahasiswa}
+                        </TableCell>
+                        <TableCell className="text-center font-medium max-w-10">
+                          {item.npm}
+                        </TableCell>
+                        <TableCell className="text-center font-medium max-w-10">
+                          {item.TA}
+                        </TableCell>
+                        <TableCell className="text-center font-medium max-w-10">
+                          <Button as={Link} href={""} variant="bordered">
+                            <img
+                              src="/icon-detail.png"
+                              alt=""
+                              className="w-10 h-10"
+                            />
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  }}
 
-                      <TableCell className="text-center font-medium">
-                        aaa
-                      </TableCell>
-                    </TableRow>
-                  )} */}
-
-                  <TableRow key={1} className="">
+                  {/* <TableRow key={1} className="">
                     <TableCell className="text-center font-medium max-w-10">
                       {1}
                     </TableCell>
@@ -253,7 +278,7 @@ export default function Home() {
                         />
                       </Button>
                     </TableCell>
-                  </TableRow>
+                  </TableRow> */}
                 </TableBody>
               </Table>
 
